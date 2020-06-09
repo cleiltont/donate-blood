d592109b7ea152ea598b239a197432dc49d28350
cleiltont <cleiltonceni2000@gmail.com>
2020-06-09 17:45:56 -0300

    Backend concluido

--- README.md
+++ README.md
@@ -1,4 +1,40 @@
 
-## Sobre
+## :bulb: Sobre
 Criando uma aplicação com javascript para aprendizado.
+Quando você cadastra-se no formulario, você entra para a lista de doadores.
 
+
+## :wrench:  Tecnologias
+- NodeJS
+- PostgreSQL
+- JavaScript
+- HTML
+- CSS
+
+## :nut_and_bolt:  Dependencias
+- express
+- nodemon
+- nunjucks
+- pg
+
+
+## :running: :dash:  Start
+1. Baixe ou clone o repositorio.
+2. Clonando: git clone https://github.com/cleiltont/donate-blood.git
+3. No arquivo server linha 13, preencha os dados com o seu banco de dados.
+4. Abra o terminal na pasta donate-blood e digite: npm start
+
+
+## :books:  Aprendizado
+- Criar um servidor com Node
+- Conectar-se co banco de dados com Node
+- Enviar informações para o banco de dados
+- Mostra dados do banco no html
+
+
+Feito com :heart: by Cleilton Timoteo
+
+...
+
+
+credits: rocketseat
--- server.js
+++ server.js
@@ -3,17 +3,62 @@ const express = require('express');
 const server = express();
 
 // Configurando servidor para aprensentar aqruivos extras
+server.use(express.static('public'));
+
+// Habilitando body do formulario
+server.use(express.urlencoded( { extended: true } ));
+
+// Configurando conexão com o banco de dados
+const { Pool } = require('pg');
+const db = new Pool({
+	user: 'postgres',
+	password: '071627',
+	host: 'localhost',
+	post: 5432,
+	database: 'doe'
+});
+
 
 // Configurando template engine
 const nunjucks = require('nunjucks');
 nunjucks.configure('./', {
-	express: server
+	express: server,
+	noCache: true,
 });
+
+
 
 // Configurando apresentação da pagina
-server.get('/', function (req, res){
-	return res.render('index.html');
+server.get('/', (req, res) => {
+	db.query("SELECT * FROM donates", (err, result) => {
+		const donors = result.rows;
+
+		return res.render('index.html', { donors });
+	});
 });
 
-// Escohendo porta
+// Pegando dados do formulario e cadastrando na lista de donors
+server.post('/', (req, res) => {
+	const name = req.body.name;
+	const email = req.body.email;
+	const blood = req.body.blood;
+
+
+	// Colocando valores dentro do banco de dados
+	const query = 
+		`INSERT INTO donates ("name", "email", "blood")
+		VALUES ($1, $2, $3)`;
+	const values = [name, email, blood]
+	
+	db.query(query, values, (err) => {
+		// Se ocorre um erro
+		if(err) return res.send('Erro no banco de dados.')
+
+		// Redirecionando para pagina inicial
+		return res.redirect('/');
+	});
+
+});
+
+// Ligando o servidor e permitindo acesso a porta 3000
 server.listen(3000);
